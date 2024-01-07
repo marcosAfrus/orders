f859f94b64b5436fdb5cbeacb07d9564e76ea3be
Jesus PM - UNIR <jesus.perezmelero@unir.net>
2024-01-07 19:00:49 +0100

    Support for initial data loading

--- src/main/java/com/unir/products/model/pojo/Product.java
+++ src/main/java/com/unir/products/model/pojo/Product.java
@@ -24,7 +24,7 @@ import lombok.ToString;
 public class Product {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	@Column(name = "name", unique = true)
---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO products (id, name, description, country, visible) VALUES (1, 'iPhone 14', 'Estupendo iPhone 14', 'ES', true);
+INSERT INTO products (id, name, description, country, visible) VALUES (2, 'iPhone 13', 'Estupendo iPhone 13', 'PT', true);
+INSERT INTO products (id, name, description, country, visible) VALUES (3, 'iPhone 12', 'Estupendo iPhone 12', 'DE', false);
+INSERT INTO products (id, name, description, country, visible) VALUES (4, 'iPhone 11', 'Estupendo iPhone 11', 'AR', true);
